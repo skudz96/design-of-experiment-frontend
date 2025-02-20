5a9ae0e04ed3da90a03a348c03b1c22af49c7bfc
Sergio <sergeh96@gmail.com>
2025-02-20 12:34:42 +0000

    first next commit. Lost commit history from the VITE build

---
+++ src/app/Components/Body.tsx
@@ -0,0 +1,13 @@
+import Form from "./Form";
+/* import Table from "./Table"; */
+
+export default function Body() {
+  return (
+    <div className="px-4 py-5 sm:p-6">
+      <h1 className="text-2x1 font-semibold text-gray-900 mb-6">
+        Experiment Design Tool
+      </h1>
+      <Form />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Body from "./Components/Body";
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <main className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-3xl mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+        <Body />
+      </div>
+    </main>
+  );
 }
